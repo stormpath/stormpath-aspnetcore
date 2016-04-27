1740d870262d42b2367f4bffb07e80bec3d6f1cb
Nate Barbettini <nathanaelb@gmail.com>
2016-04-27 11:27:05 -0700

    Add documentation examples project

---
+++ docs/Stormpath.AspNetCore.DocExamples/Controllers/ServicesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Stormpath.SDK.Account;
+using Stormpath.SDK.Application;
+
+namespace Stormpath.AspNetCore.DocExamples.Controllers
+{
+    #region code/csharp/injecting_objects/controller_fromservices.cs
+    public class ServicesController : Controller
+    {
+        [FromServices]
+        public IApplication StormpathApplication { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    #endregion
+
+    #region code/csharp/injecting_objects/controller_injection.cs
+    public class InjectedServicesController : Controller
+    {
+        public IApplication StormpathApplication { get; private set; }
+
+        public InjectedServicesController(IApplication stormpathApplication)
+        {
+            this.StormpathApplication = stormpathApplication;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    #endregion
+
+    #region code/csharp/injecting_objects/injecting_user.cs
+    public class UserController : Controller
+    {
+        [FromServices]
+        public Lazy<IAccount> Account { get; set; }
+
+        public async Task<IActionResult> Index()
+        {
+            // If the request is authenticated, do something with the account
+            // (like get the account's Custom Data):
+            if (Account.Value != null)
+            {
+                var customData = await Account.Value.GetCustomDataAsync();
+            }
+
+            return View();
+        }
+    }
+    #endregion
+}
---
+++ docs/Stormpath.AspNetCore.DocExamples/Startup_Configuration.cs
@@ -0,0 +1,123 @@
+using Microsoft.Extensions.DependencyInjection;
+using Stormpath.Configuration.Abstractions;
+
+namespace Stormpath.AspNetCore.DocExamples
+{
+    public class Startup_Configuration
+    {
+
+        public void ConfigureServices_Model(IServiceCollection services)
+        {
+            #region code/csharp/configuration/inline_config.cs
+            services.AddStormpath(new StormpathConfiguration()
+            {
+                Application = new ApplicationConfiguration()
+                {
+                    Name = "My Application"
+                },
+                Web = new WebConfiguration()
+                {
+                    Register = new WebRegisterRouteConfiguration()
+                    {
+                        Enabled = false
+                    }
+                }
+            });
+            #endregion
+        }
+
+        public void ConfigureServices_AnonymousModel(IServiceCollection services)
+        {
+            #region code/csharp/configuration/inline_config.cs
+            services.AddStormpath(new
+            {
+                application = new
+                {
+                    name = "My Application"
+                },
+                web = new
+                {
+                    register = new
+                    {
+                        enabled = false
+                    }
+                }
+            });
+            #endregion
+        }
+
+        public void ConfigureServices_DisableDefaultFeatures(IServiceCollection services)
+        {
+            #region code/csharp/configuration/disable_default_features.cs
+            services.AddStormpath(new StormpathConfiguration()
+            {
+                Web = new WebConfiguration()
+                {
+                    ForgotPassword = new WebForgotPasswordRouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    ChangePassword = new WebChangePasswordRouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    Login = new WebLoginRouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    Logout = new WebLogoutRouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    Me = new WebMeRouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    Oauth2 = new WebOauth2RouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    Register = new WebRegisterRouteConfiguration()
+                    {
+                        Enabled = false
+                    },
+                    VerifyEmail = new WebVerifyEmailRouteConfiguration()
+                    {
+                        Enabled = false
+                    }
+                }
+            });
+            #endregion
+        }
+
+        public void ConfigureServices_ApiCredentials(IServiceCollection services)
+        {
+            #region code/csharp/configuration/api_credentials.cs
+            services.AddStormpath(new StormpathConfiguration()
+            {
+                Client = new ClientConfiguration()
+                {
+                    ApiKey = new ClientApiKeyConfiguration()
+                    {
+                        Id = "YOUR_API_KEY_ID",
+                        Secret = "YOUR_API_KEY_SECRET"
+                    }
+                }
+            });
+            #endregion
+        }
+
+        public void ConfigureServices_DisableHtml(IServiceCollection services)
+        {
+            #region code/csharp/configuration/disable_html_produces.cs
+            services.AddStormpath(new StormpathConfiguration()
+            {
+                Web = new WebConfiguration()
+                {
+                    Produces = new string[] { "application/json" }
+                }
+            });
+            #endregion
+        }
+    }
+}
