633d1de52474130899c50a410ae47fc15557c14f
Nate Barbettini <nathanaelb@gmail.com>
2016-05-19 15:58:51 -0700

    Adding more doc code

--- docs/Stormpath.AspNetCore.DocExamples/Controllers/ServicesController.cs
+++ docs/Stormpath.AspNetCore.DocExamples/Controllers/ServicesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
+using Stormpath.SDK;
 using Stormpath.SDK.Account;
 using Stormpath.SDK.Application;
 
@@ -38,22 +40,27 @@ namespace Stormpath.AspNetCore.DocExamples.Controllers
     }
     #endregion
 
-    #region code/request_objects/aspnetcore/injecting_user.cs
-    public class UserController : Controller
+    #region code/request_objects/aspnetcore/injecting_application.cs
+    public class AccountsController : Controller
     {
         [FromServices]
-        public Lazy<IAccount> Account { get; set; }
-
-        public async Task<IActionResult> Index()
+        public IApplication StormpathApplication { get; set; }
+        
+        [HttpGet]
+        public async Task<IActionResult> FindAccountByEmail(string email)
         {
-            // If the request is authenticated, do something with the account
-            // (like get the account's Custom Data):
-            if (Account.Value != null)
-            {
-                var customData = await Account.Value.GetCustomDataAsync();
-            }
+            var foundAccount = await StormpathApplication.GetAccounts()
+                     .Where(a => a.Email == email)
+                     .SingleOrDefaultAsync();
 
-            return View();
+            if (foundAccount == null)
+            {
+                return Ok("No accounts found.");
+            }
+            else
+            {
+                return Ok(foundAccount.FullName);
+            }
         }
     }
     #endregion
@@ -64,8 +71,8 @@ namespace Stormpath.AspNetCore.DocExamples.Controllers
         [FromServices]
         public Lazy<IAccount> Account { get; set; }
 
-        
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> UpdatePassword(string newPassword)
         {
             if (Account.Value != null)
