357a199854baaf9c3939e3f69d43728938179a1c
Nate Barbettini <nathanaelb@gmail.com>
2016-06-14 15:42:09 -0700

    Add views to test project

---
+++ test/Stormpath.AspNetCore.TckHarness/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stormpath.AspNetCore.TestHarness.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- test/Stormpath.AspNetCore.TckHarness/Startup.cs
+++ test/Stormpath.AspNetCore.TckHarness/Startup.cs
@@ -63,7 +63,12 @@ namespace Stormpath.AspNetCore.TestHarness
             // Use Stormpath middleware
             app.UseStormpath();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
