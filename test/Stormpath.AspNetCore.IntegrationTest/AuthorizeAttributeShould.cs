4ca5543e02ac28569fe1205ca813ad57af3f5b63
Nate Barbettini <nathanaelb@gmail.com>
2016-10-08 20:13:08 -0700

    Flesh out AuthorizeAttribute tests

--- test/Stormpath.AspNetCore.IntegrationTest/AuthorizeAttributeShould.cs
+++ test/Stormpath.AspNetCore.IntegrationTest/AuthorizeAttributeShould.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Stormpath.SDK.Oauth;
 using Xunit;
 
 namespace Stormpath.AspNetCore.IntegrationTest
@@ -46,6 +47,188 @@ namespace Stormpath.AspNetCore.IntegrationTest
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task AllowBrowserRequestAuthorizedWithHeader()
+        {
+            // Arrange
+            var server = TestServerInstance.Create(_fixture);
+
+            using (var cleanup = new AutoCleanup(_fixture.Client))
+            {
+                var email = $"its-{_fixture.TestKey}@example.com";
+                var account = await _fixture.TestApplication.CreateAccountAsync(
+                    nameof(AllowBrowserRequestAuthorizedWithHeader),
+                    nameof(AuthorizeAttributeShould),
+                    email,
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account);
+
+                var grantRequest = OauthRequests.NewPasswordGrantRequest()
+                    .SetAccountStore(_fixture.TestDirectory)
+                    .SetLogin(email)
+                    .SetPassword("Changeme123!!")
+                    .Build();
+                var grantResponse = await _fixture.TestApplication.NewPasswordGrantAuthenticator()
+                    .AuthenticateAsync(grantRequest);
+
+                // Act
+                var request = new HttpRequestMessage(HttpMethod.Get, "/protected");
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", grantResponse.AccessTokenString);
+                var response = await server.SendAsync(request);
+
+                // Assert
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+
+        [Fact]
+        public async Task AllowBrowserRequestAuthorizedWithCookie()
+        {
+            // Arrange
+            var server = TestServerInstance.Create(_fixture);
+
+            using (var cleanup = new AutoCleanup(_fixture.Client))
+            {
+                var email = $"its-{_fixture.TestKey}@example.com";
+                var account = await _fixture.TestApplication.CreateAccountAsync(
+                    nameof(AllowBrowserRequestAuthorizedWithHeader),
+                    nameof(AuthorizeAttributeShould),
+                    email,
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account);
+
+                var grantRequest = OauthRequests.NewPasswordGrantRequest()
+                    .SetAccountStore(_fixture.TestDirectory)
+                    .SetLogin(email)
+                    .SetPassword("Changeme123!!")
+                    .Build();
+                var grantResponse = await _fixture.TestApplication.NewPasswordGrantAuthenticator()
+                    .AuthenticateAsync(grantRequest);
+
+                // Act
+                var request = new HttpRequestMessage(HttpMethod.Get, "/protected");
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                request.Headers.Add("Cookie", $"access_token={grantResponse.AccessTokenString}");
+                var response = await server.SendAsync(request);
+
+                // Assert
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+
+        [Fact]
+        public async Task AllowJsonRequestAuthorizedWithHeader()
+        {
+            // Arrange
+            var server = TestServerInstance.Create(_fixture);
+
+            using (var cleanup = new AutoCleanup(_fixture.Client))
+            {
+                var email = $"its-{_fixture.TestKey}@example.com";
+                var account = await _fixture.TestApplication.CreateAccountAsync(
+                    nameof(AllowBrowserRequestAuthorizedWithHeader),
+                    nameof(AuthorizeAttributeShould),
+                    email,
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account);
+
+                var grantRequest = OauthRequests.NewPasswordGrantRequest()
+                    .SetAccountStore(_fixture.TestDirectory)
+                    .SetLogin(email)
+                    .SetPassword("Changeme123!!")
+                    .Build();
+                var grantResponse = await _fixture.TestApplication.NewPasswordGrantAuthenticator()
+                    .AuthenticateAsync(grantRequest);
+
+                // Act
+                var request = new HttpRequestMessage(HttpMethod.Get, "/protected");
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", grantResponse.AccessTokenString);
+                var response = await server.SendAsync(request);
+
+                // Assert
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+
+        [Fact]
+        public async Task AllowJsonRequestAuthorizedWithCookie()
+        {
+            // Arrange
+            var server = TestServerInstance.Create(_fixture);
+
+            using (var cleanup = new AutoCleanup(_fixture.Client))
+            {
+                var email = $"its-{_fixture.TestKey}@example.com";
+                var account = await _fixture.TestApplication.CreateAccountAsync(
+                    nameof(AllowBrowserRequestAuthorizedWithHeader),
+                    nameof(AuthorizeAttributeShould),
+                    email,
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account);
+
+                var grantRequest = OauthRequests.NewPasswordGrantRequest()
+                    .SetAccountStore(_fixture.TestDirectory)
+                    .SetLogin(email)
+                    .SetPassword("Changeme123!!")
+                    .Build();
+                var grantResponse = await _fixture.TestApplication.NewPasswordGrantAuthenticator()
+                    .AuthenticateAsync(grantRequest);
+
+                // Act
+                var request = new HttpRequestMessage(HttpMethod.Get, "/protected");
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("Cookie", $"access_token={grantResponse.AccessTokenString}");
+                var response = await server.SendAsync(request);
+
+                // Assert
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+
+        [Fact]
+        public async Task HandleConcurrentRequests()
+        {
+            // Arrange
+            var server = TestServerInstance.Create(_fixture);
+
+            using (var cleanup = new AutoCleanup(_fixture.Client))
+            {
+                var email = $"its-{_fixture.TestKey}@example.com";
+                var account = await _fixture.TestApplication.CreateAccountAsync(
+                    nameof(AllowBrowserRequestAuthorizedWithHeader),
+                    nameof(AuthorizeAttributeShould),
+                    email,
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account);
+
+                var grantRequest = OauthRequests.NewPasswordGrantRequest()
+                    .SetAccountStore(_fixture.TestDirectory)
+                    .SetLogin(email)
+                    .SetPassword("Changeme123!!")
+                    .Build();
+                var grantResponse = await _fixture.TestApplication.NewPasswordGrantAuthenticator()
+                    .AuthenticateAsync(grantRequest);
+
+                // Act
+                var request1 = new HttpRequestMessage(HttpMethod.Get, "/protected");
+                request1.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                request1.Headers.Add("Cookie", $"access_token={grantResponse.AccessTokenString}");
+
+                var request2 = new HttpRequestMessage(HttpMethod.Get, "/protected");
+                request2.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+
+                var responses = await Task.WhenAll(
+                    server.SendAsync(request1),
+                    server.SendAsync(request2));
+
+                // Assert
+                responses[0].StatusCode.Should().Be(HttpStatusCode.OK);
+                responses[1].StatusCode.Should().Be(HttpStatusCode.Redirect);
+            }
         }
     }
 }
