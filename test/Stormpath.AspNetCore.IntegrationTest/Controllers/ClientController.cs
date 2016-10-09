34426b25980eb181385d1f10a30895bbb1d3af76
Nate Barbettini <nathanaelb@gmail.com>
2016-10-09 12:03:07 -0700

    Test getting the client from the context

---
+++ test/Stormpath.AspNetCore.IntegrationTest/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Stormpath.SDK.Client;
+
+namespace Stormpath.AspNetCore.IntegrationTest.Controllers
+{
+    [Route("/client")]
+    public class ClientController : Controller
+    {
+        public readonly IClient _client;
+
+        public ClientController(IClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var tenant = await _client.GetCurrentTenantAsync();
+
+            return Ok(tenant.Href);
+        }
+    }
+}
