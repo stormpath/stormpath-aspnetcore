dfda8bc346809c7b46460787c6f7b1b7b2306347
Nate Barbettini <nathanaelb@gmail.com>
2016-10-09 11:41:42 -0700

    Add a concurrent test

--- test/Stormpath.AspNetCore.IntegrationTest/GroupsRequirementShould.cs
+++ test/Stormpath.AspNetCore.IntegrationTest/GroupsRequirementShould.cs
@@ -179,5 +179,63 @@ namespace Stormpath.AspNetCore.IntegrationTest
                 response.StatusCode.Should().Be(HttpStatusCode.OK);
             }
         }
+
+        [Fact]
+        public async Task HandleConcurrentRequests()
+        {
+            // Arrange
+            var server = TestServerInstance.Create(_fixture,
+                services =>
+                {
+                    services.AddAuthorization(opt =>
+                    {
+                        opt.AddPolicy("AdminITGroup",
+                            policy => policy.AddRequirements(new StormpathGroupsRequirement("adminIT")));
+                    });
+                });
+
+            using (var cleanup = new AutoCleanup(_fixture.Client))
+            {
+                var group = await _fixture.TestDirectory.CreateGroupAsync("adminIT", "Stormpath.AspNetCore test group");
+                cleanup.MarkForDeletion(group);
+
+                var email = $"its-{_fixture.TestKey}@example.com";
+                var account = await _fixture.TestApplication.CreateAccountAsync(
+                    nameof(HandleConcurrentRequests),
+                    nameof(GroupsRequirementShould),
+                    email,
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account);
+                await account.AddGroupAsync(group);
+
+                var account2 = await _fixture.TestApplication.CreateAccountAsync(
+                    $"{nameof(HandleConcurrentRequests)} #2",
+                    nameof(GroupsRequirementShould),
+                    $"its-{_fixture.TestKey}-2@example.com",
+                    "Changeme123!!");
+                cleanup.MarkForDeletion(account2);
+
+                var accessToken1 = await _fixture.GetAccessToken(account, "Changeme123!!");
+                var accessToken2 = await _fixture.GetAccessToken(account2, "Changeme123!!");
+
+                var request1 = new HttpRequestMessage(HttpMethod.Get, "/requireGroup");
+                request1.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                request1.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken1);
+
+                var request2 = new HttpRequestMessage(HttpMethod.Get, "/requireGroup");
+                request2.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                request2.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken2);
+
+                // Act
+                var responses = await Task.WhenAll(
+                    server.SendAsync(request1),
+                    server.SendAsync(request2));
+
+                // Assert
+                responses[0].StatusCode.Should().Be(HttpStatusCode.OK);
+
+                responses[1].StatusCode.Should().Be(HttpStatusCode.Redirect);
+            }
+        }
     }
 }
