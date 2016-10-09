0279e0051b82d7fc56672017041cf0a817aed054
Nate Barbettini <nathanaelb@gmail.com>
2016-10-09 13:04:05 -0700

    Add test for custom data requirement

---
+++ test/Stormpath.AspNetCore.IntegrationTest/Controllers/RequireCustomDataController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stormpath.AspNetCore.IntegrationTest.Controllers
+{
+    [Route("/requireCustomData")]
+    [Authorize(Policy = "CustomDataIT")]
+    public class RequireCustomDataController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
