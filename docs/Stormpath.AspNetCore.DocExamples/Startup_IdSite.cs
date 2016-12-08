bae12cf25918abe21675ddeb8bbdf03eb94aa6ad
Nate Barbettini <nathanaelb@gmail.com>
2016-12-08 11:10:05 -0800

    Add ID Site example

---
+++ docs/Stormpath.AspNetCore.DocExamples/Startup_IdSite.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Stormpath.Configuration.Abstractions;
+
+namespace Stormpath.AspNetCore.DocExamples
+{
+    public class Startup_IdSite
+    {
+        public void ConfigureServices_IdSite(IServiceCollection services)
+        {
+            #region code/id_site/aspnetcore/enable_idsite.cs
+            services.AddStormpath(new StormpathConfiguration()
+            {
+                Web = new WebConfiguration()
+                {
+                    IdSite = new WebIdSiteConfiguration()
+                    {
+                        Enabled = true
+                    }
+                }
+            });
+            #endregion
+        }
+    }
+}
