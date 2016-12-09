573eb0778a8af403bddfca2f2c14733eb44bf1e7
Nate Barbettini <nathanaelb@gmail.com>
2016-12-08 18:56:06 -0800

    Add post-login handler example

---
+++ docs/Stormpath.AspNetCore.DocExamples/Startup_PostLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Stormpath.Configuration.Abstractions;
+
+namespace Stormpath.AspNetCore.DocExamples
+{
+    public class Startup_PostLogin
+    {
+        public void ConfigureServices_Basic(IServiceCollection services)
+        {
+            #region code/login/aspnetcore/postlogin_handler.cs
+            services.AddStormpath(new StormpathOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                PostLoginHandler = (context, ct) =>
+                {
+                    return Task.FromResult(0);
+                }
+            });
+            #endregion
+        }
+    }
+}
