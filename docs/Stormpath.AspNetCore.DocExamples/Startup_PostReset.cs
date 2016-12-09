69c1a59f42cae96369afd16eb65da4c138a164b9
Nate Barbettini <nathanaelb@gmail.com>
2016-12-09 11:51:39 -0800

    Add pre/post reset password examples

---
+++ docs/Stormpath.AspNetCore.DocExamples/Startup_PostReset.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Stormpath.Configuration.Abstractions;
+
+namespace Stormpath.AspNetCore.DocExamples
+{
+    public class Startup_PostReset
+    {
+        public void ConfigureServices_Basic(IServiceCollection services)
+        {
+            #region code/password_reset/aspnetcore/postreset_handler.cs
+            services.AddStormpath(new StormpathOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                PostChangePasswordHandler = (context, ct) =>
+                {
+                    return Task.FromResult(0);
+                }
+            });
+            #endregion
+        }
+    }
+}
